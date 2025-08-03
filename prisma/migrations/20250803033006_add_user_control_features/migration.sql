bc2601e893e5768fd7735be5632ee71e744fd1a8
gustavoandres2021corrientes-stack <gustavoandres2021corrientes@gmail.com>
2025-08-03 18:41:04 -0300

    Delete prisma directory

--- prisma/migrations/20250803033006_add_user_control_features/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "demoExpiresAt" TIMESTAMP(3),
-ADD COLUMN     "status" TEXT NOT NULL DEFAULT 'active',
-ADD COLUMN     "suspendedAt" TIMESTAMP(3),
-ADD COLUMN     "suspensionReason" TEXT,
-ADD COLUMN     "userType" TEXT NOT NULL DEFAULT 'standard';
